99a9c1f5b3e2d1836e87a6a31b8fe7f8e70fc9f9
hrasel <59330627+hrasel@users.noreply.github.com>
2023-01-26 17:28:45 +0600

    Initial uploaded

---
+++ app.js
@@ -0,0 +1,49 @@
+// Shrink navbar on scroll
+const navBar = document.getElementById('main_navbar');
+window.onscroll = () => {
+    if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100 ) {
+        navBar.classList.add('scrolled');
+        
+    } else {
+        navBar.classList.remove('scrolled');
+    }
+}
+
+// Smooth Scroll
+// Smooth Scrolling
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
